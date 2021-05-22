36f85398395b4701146e94ee85a0b00d5e578a15
Ishaan Gupta <ishaangupta@Ishaans-MacBook-Pro-2.local>
2021-05-22 13:55:00 -0700

    makeblastdb

--- scripts/mkdb.sh
+++ scripts/mkdb.sh
@@ -1,1 +1,16 @@
-makeblastdb -in 
+makeblastdb \
+	-in ../Datasets/protein_db.fasta \
+	-input_type fasta\
+#	-dbtype prot is default
+	-title "cov2_ref_proteins"
+	-parse_seqids
+# 	-hash_index not needed
+
+makeblastdb \
+	-in ../Datasets/nucleotide_sequences.fasta \
+	-input_type "fasta"\
+	-dbtype nucl
+	-title "cov2_queries"
+	-parse_seqids
+# 	-hash_index not needed
+	
